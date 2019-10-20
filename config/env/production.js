e2e20d26951a3895bee49a0c1cbaf9d113981110
Alvarado <alfelix@amazon.com>
2019-10-20 08:41:28 -0700

    turned on CORS in config/env/production.js

--- config/env/production.js
+++ config/env/production.js
@@ -147,11 +147,11 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // cors: {
-    //   allowOrigins: [
-    //     '*',
-    //   ]
-    // },
+    cors: {
+      allowOrigins: [
+        '*',
+      ]
+    },
 
   },
 
