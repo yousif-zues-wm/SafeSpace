7079bd8aeda8615092e01cd3562c54406ceca78a
Alvarado <alfelix@amazon.com>
2019-10-20 08:36:05 -0700

    turned on CORS in security.js

--- config/security.js
+++ config/security.js
@@ -31,6 +31,7 @@ module.exports.security = {
   cors: {
     allRoutes: true,
     allowOrigins: '*',
+    allowCredentials: false
   }
 
   /****************************************************************************
